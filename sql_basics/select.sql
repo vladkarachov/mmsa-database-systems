2d9b77e5d1125b9845d18a04e290dacb20667a28
Egor Zagny <egor.zagny@gmail.com>
2019-09-28 00:38:11 +0300

    add selected.sql

---
+++ sql_basics/select.sql
@@ -0,0 +1,3 @@
+﻿SELECT p.first_name, p.last_name, COUNT(o.id), SUM(oi.quantity), SUM(oi.quantity*i.price-oi.discount)
+FROM `person` p LEFT JOIN `order` o ON p.id = o.person_id LEFT JOIN `order_item` oi ON o.id = oi.order_id
+LEFT JOIN `item` i ON oi.item_id = i.id GROUP BY p.id
